1c385fa60f8e9dbfc37b1fecc777fa2ef5614115
Karim DRIDI <seth.holth@gmail.com>
2025-03-09 16:51:56 +0100

    Implement bird flying mechanics and game state management in main scene

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -1,9 +1,30 @@
 extends CharacterBody2D
 
-const START_POSITION = Vector2(100, 400)
+const GRAVITY : int = 1000
+const START_POSITION : Vector2 = Vector2(100, 400)
+const FLAP_SPEED : int = -500
+const MAX_VELOCITY : int = 600
+
+var flying : bool
 
 func _ready() -> void:
 	reset()
 
+func _physics_process(delta: float) -> void:
+	if flying:
+		velocity.y += GRAVITY * delta
+		if velocity.y > MAX_VELOCITY:
+			velocity.y = MAX_VELOCITY
+		set_rotation(deg_to_rad(velocity.y * 0.05))
+		$AnimatedSprite2D.play()
+		move_and_collide(velocity * delta)
+	else:
+		$AnimatedSprite2D.stop()
+
 func reset() -> void:
+	flying = false
 	position = START_POSITION
+	set_rotation(0)
+
+func flap() -> void:
+	velocity.y = FLAP_SPEED
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,7 +1,27 @@
 extends Node2D
 
+var game_over
+var game_running
+
 func _ready() -> void:
 	new_game()
 
 func new_game() -> void:
+	game_over = false
+	game_running = false
 	$Bird.reset()
+
+func _input(event: InputEvent) -> void:
+	if game_over == false:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				if game_running == false:
+					start_game()
+				else:
+					if $Bird.flying:
+						$Bird.flap()
+
+func start_game() -> void:
+	game_running = true
+	$Bird.flying = true
+	$Bird.flap()
