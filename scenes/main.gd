30dd2d99440db8b21d47cdec618293c171177fe1
Karim DRIDI <seth.holth@gmail.com>
2025-03-09 17:28:56 +0100

    Add pipe generation and scrolling functionality to main scene

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
+@export var pipe_scene : PackedScene
+
 const SCROLL_SPEED : int = 5
+const PIPE_DELAY : int = 100
+const PIPE_RANGE : int = 200
 
 var game_over : bool
 var game_running : bool
 var screen_size : Vector2i
+var groung_height : int
 var scroll : int
+var pipes : Array
 
 func _ready() -> void:
 	screen_size = get_window().size
+	groung_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func _input(event: InputEvent) -> void:
@@ -27,14 +34,29 @@ func _process(delta: float) -> void:
 		if scroll >= screen_size.x:
 			scroll -= screen_size.x
 		$Ground.position.x = -scroll
+		for pipe in pipes:
+			pipe.position.x -= SCROLL_SPEED
+
+func _on_pipe_timer_timeout() -> void:
+	generate_pipes()
 
 func new_game() -> void:
 	game_over = false
 	game_running = false
 	scroll = 0
+	pipes.clear()
+	generate_pipes()
 	$Bird.reset()
 
 func start_game() -> void:
 	game_running = true
 	$Bird.flying = true
 	$Bird.flap()
+	$PipeTimer.start()
+
+func generate_pipes() -> void:
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + PIPE_DELAY
+	pipe.position.y = (screen_size.x - groung_height) / 2 + randi_range(-PIPE_RANGE, PIPE_RANGE)
+	add_child(pipe)
+	pipes.append(pipe)
